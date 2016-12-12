da50bde2dda8ef40c03c07b61f5f38e1549c54e5
makobasuri <makobasuri@gmail.com>
2016-12-12 13:11:16 +0100

    added files for project

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+    return gulp.src('style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe('gulp.dest'('style.css'))
+        .pipe(sync.stream());
+});
+
+gulp.task('serve', function () {
+    sync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch('style.scss', ['sass']);
+    gulp.watch("*.html").on('change', sync.reload);
+});
+
+gulp.task('default', ['serve']);
