555c739d9cf01ba73a6c2370ef54fff63a9abbb8
makobasuri <makobasuri@gmail.com>
2016-12-15 11:34:48 +0100

    added basic support for variable img-ratios

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,48 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    function galleryEffect() {
+    function galEffect() {
 
     }
 
-    function firstImage() {
+    function galDrawImage(imgSrc) {
 
-    }
-
-    function drawImage(imgSrc) {
-
-        var canvas = document.getElementById('myCanvas');
+        var canvas = document.getElementById('gal--myCanvas');
 
         if(canvas.getContext) {
             var ctx = canvas.getContext('2d');
-            var imgObject = new Image();
+            var imgObject = new Image();         
             imgObject.src = imgSrc;
+
+            if (imgObject.width != canvas.width)
+                canvas.width = imgObject.width;
+            if (imgObject.height != canvas.height)
+                canvas.height = imgObject.height;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.drawImage(imgObject, 0, 0);
         } else {
             alert("Can't get context!");
         }
     }
+
+    function galFirstImage() {
+        var images = document.getElementsByClassName('gal--img');
+        var img = images[0].getAttribute('src');
+        galDrawImage(img);
+    }
+    galFirstImage();
     
-    document.getElementById('gallery--thumbs-container').addEventListener('click', function(event) {
-        console.log(event);
+    function galChangeImage() {
+        document.getElementById('gal--thumbs-container').addEventListener('click', function(event) {
 
-        event = event || window.event;
-        console.log(event.target);
+            var targetElement = event.target || event.srcElement;
 
-        var targetElement = event.target || event.srcElement;
-
-        if (targetElement.tagName == 'IMG') {
-            var img = targetElement.getAttribute('src');
-            console.log('test' + img);
-            drawImage(img);
-        }
-    });
+            if (targetElement.tagName == 'IMG') {
+                var img = targetElement.getAttribute('src');
+                console.log('test' + img);
+                galDrawImage(img);
+            }
+        });
+    }
+    galChangeImage();
 });
