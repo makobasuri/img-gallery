8112fbd33633b386ba634da481425b2d2159b5b8
makobasuri <makobasuri@gmail.com>
2016-12-16 13:29:57 +0100

    added sourcemaps and completed basic alpha animation

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,23 @@ var sass = require('gulp-sass');
 var sync = require('browser-sync').create();
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     return gulp.src('src/scss/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/css/'))
         .pipe(sync.stream());
 });
 
 gulp.task('scripts', function () {
     return gulp.src('src/js/*.js')
+        .pipe(sourcemaps.init())
         .pipe(concat('main.js'))
         .pipe(uglify())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/js/'))
         .pipe(sync.stream());
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,50 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    
+    var canvas = document.getElementById('gal--myCanvas');
+    var ctx = canvas.getContext('2d');
+    var imgObject = new Image();
+    var cw = canvas.width;
+    var ch = canvas.height;
+    var iw = imgObject.width;
+    var ih = imgObject.height;
+    var animPctComplete;
+    var fps = 60;
 
     function galDrawImage(imgSrc) {
-
-        var canvas = document.getElementById('gal--myCanvas');
-
-        var ctx = canvas.getContext('2d');
-        var imgObject = new Image();
         
         imgObject.onload = function() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(imgObject, 0, 0);
+            animPctComplete = 0;
+            galAnimate();
         };
 
         imgObject.src = imgSrc;
+    }
 
-        if (imgObject.width != canvas.width)
-            canvas.width = imgObject.width;
-        if (imgObject.height != canvas.height)
-            canvas.height = imgObject.height;
+    function galAnimate() {
+        setTimeout(function() {
+            
+            ctx.globalAlpha = animPctComplete;
+            ctx.drawImage(imgObject, 0, 0);
+            animPctComplete += 0.01;
+            console.log(animPctComplete);
+
+            if(Math.floor(animPctComplete) !== 1) {
+                requestAnimationFrame(galAnimate);
+            }
+
+        }, 1000 / fps);
     }
 
     function galFirstImage() {
         var images = document.getElementsByClassName('gal--img');
         var img = images[0].getAttribute('src');
+        
         galDrawImage(img);
     }
     galFirstImage();
 
     function galChangeImage() {
         document.getElementById('gal--thumbs-container').addEventListener('click', function(event) {
-
             var targetElement = event.target || event.srcElement;
 
             if (targetElement.tagName == 'IMG') {
