ed841eb7bf1fb8504c6aca3c5077e93ed31788de
makobasuri <makobasuri@gmail.com>
2016-12-16 08:48:17 +0100

    fixed order of statements in drawing function

--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var canvas = document.getElementById('gal--myCanvas');
 
         var ctx = canvas.getContext('2d');
-        var imgObject = new Image();         
+        var imgObject = new Image();
+        
+        imgObject.onload = function() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(imgObject, 0, 0);
+        };
+
         imgObject.src = imgSrc;
 
         if (imgObject.width != canvas.width)
             canvas.width = imgObject.width;
         if (imgObject.height != canvas.height)
             canvas.height = imgObject.height;
-        
-        imgObject.onload = function() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(imgObject, 0, 0);
-        };    
     }
 
     function galFirstImage() {
